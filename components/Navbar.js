77300f0d29c6edd5c211d99069fdddf594ed5ca7
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-05-14 10:52:13 +0200

    fix error: png not showing

--- components/Navbar.js
+++ components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
       <div className="nav--center">
         <div className="nav--header">
             <Image 
-              src="/signature.png" 
+              src="../public/images/signature1.png" 
               className="logo"
               alt="logo" 
               width={150}
