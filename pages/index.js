417c424f6cb558ab6b38cc864fd5e8ffb88009ed
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-05-14 12:02:04 +0200

    fix error: png not showing

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         <title>Matthew Harrison</title>
         <meta name="description" content="Matthew Harrison" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./favicon.ico" />
       </Head>
       <main>
         <Navbar />
